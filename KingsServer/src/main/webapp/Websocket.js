8070bd5460dd72a74061785a670e81d57be09916
otsuka <slowird7@gmail.com>
2024-11-30 00:36:01 +0900

    localhost 決め打ちを止めた

--- KingsServer/src/main/webapp/Websocket.js
+++ KingsServer/src/main/webapp/Websocket.js
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 
-//var wsUri = "ws://" + document.location.host + document.location.pathname + "endpoint";
-var wsUri = "ws://localhost:8080/kingserver/endpoint";
+var pathArray = document.location.pathname.split('/');
+pathArray.pop(); // Remove the last element to get the parent path
+var parentPath = pathArray.join('/');
+var wsUri = "ws://" + document.location.host + parentPath + "/endpoint";
+//var wsUri = "ws://localhost:8080/kingserver/endpoint";
 var websocket = new WebSocket(wsUri);
 
 websocket.onerror = function(evt) { onError(evt); };
